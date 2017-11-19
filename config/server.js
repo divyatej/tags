eacbbb227e4cf3c636d1c82c9c1c10522420752d
Divyatej Akella <divyatej.akella@qantas.com.au>
2017-11-20 10:30:11 +1100

    Initial commit with base structure

---
+++ config/server.js
@@ -0,0 +1,43 @@
+var port = process.env.PORT || 8080;
+var serverUrl = "";
+var http = require("http");
+var path = require("path");
+var fs = require("fs");
+var getFile=function(localPath, res, mimeType) {
+	fs.readFile(localPath, function(err, contents) {
+		if(!err) {
+			res.setHeader("Content-Length", contents.length);
+			if (mimeType != undefined) {
+				res.setHeader("Content-Type", mimeType);
+			}
+			res.statusCode = 200;
+			res.end(contents);
+		} else {
+			res.writeHead(500);
+			res.end();
+		}
+	});
+}
+http.createServer( function(req, res) {
+	var filename = req.url;
+	var params = filename.substring(filename.indexOf('?')+1,filename.length);
+	filename=filename.replace('?'+params,'');
+	var ext = path.extname(filename);
+	var localPath = __dirname;
+	var validExtensions = {
+		".html" : "text/html",
+		".js": "application/javascript",
+		".css": "text/css",
+		".ico": "image/ico"
+	};
+	var mimeType = validExtensions[ext];
+	localPath += filename;
+	fs.exists(localPath, function(exists) {
+			if(exists) {
+				getFile(localPath, res, mimeType);
+			} else {
+				res.writeHead(404);
+				res.end();
+			}
+	});
+}).listen(port, serverUrl);
